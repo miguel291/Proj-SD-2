6e16964e0902e79638db847e0242f26660905962
Jopdionisio <jpedrodionisio@gmail.com>
2022-06-01 22:06:35 +0100

    role a 0 sem login

--- src/main/java/com/sdProj/demo/UserRepository.java
+++ src/main/java/com/sdProj/demo/UserRepository.java
@@ -1,6 +1,8 @@
 package com.sdProj.demo;
 
 import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
@@ -16,6 +18,9 @@ public interface UserRepository extends CrudRepository<User, String>   {
     @Query(value = "select role from users where username = ?1", nativeQuery = true)
     Integer getRole(String username);
     
+    
+    @Modifying
+    @org.springframework.transaction.annotation.Transactional
     @Query(value = "update users set role = ?2 where username = ?1", nativeQuery = true)
     void setRole(String username, int role);
  } 
